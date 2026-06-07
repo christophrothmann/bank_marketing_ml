4c8e86677104dd6e444b4832fd6258bf03a74056
Christoph Rothmann <christoph.rothmann@stud.th-deg.de>
2026-06-07 17:06:13 +0200

    feat: datenaufteilung für das modeltraining

--- source/models/ModelData.R
+++ source/models/ModelData.R
@@ -3,8 +3,6 @@ library(here)
 # Set global options
 options(scipen = 999)
 
-## SST Daten für das Training der verschiedenen Modelle
-
 Daten <- read.csv(
     here::here("data", "bank-full.csv"),
     header = TRUE,
@@ -13,13 +11,23 @@ Daten <- read.csv(
     stringsAsFactors = TRUE
 )
 
-# Den gesamten Datensatz mischen -> NEEEE, evtl. nicht, weil Zeitreihen vorhanden sind
-# Folgendes Paket in betracht ziehen: https://cran.r-project.org/web/packages/prophet/index.html
-daten_shuffled <- Daten[sample(nrow(Daten)), ]
+# Variable "duration" entfernen, da kaum aussagekraft über Ausgang von "y" hat.
+Daten <- Daten[, !names(Daten) %in% "duration"]
 
-# Daten aufteilen (erste 80% für Training, restliche 20% für Test)
-train_groesse <- round(nrow(daten_shuffled) * 0.8)
-train_data <- daten_shuffled[1:train_groesse, ]
+# Chronologische Aufteilung ohne Mischen, da Zeitreihen vorliegen.
+N <- nrow(Daten)
 
-# -> Kapitel 10.6 Testdatensatz bei Zeitreihen aus Vorlesungsfolie beachten!! -> aktuell noch nicht integriert
-test_data <- daten_shuffled[(train_groesse + 1):nrow(daten_shuffled), ]
+# --- 1. Standard-Aufteilung (70% Training / 30% Test) ---
+train_groesse_70 <- round(N * 0.7)
+
+train_data <- Daten[1:train_groesse_70, ]
+test_data <- Daten[(train_groesse_70 + 1):N, ]
+
+# --- 2. Dreiteilung (50% Training / 20% Validierung / 30% Test) ---
+train_groesse_50 <- round(N * 0.5)
+val_groesse_20 <- round(N * 0.2)
+val_end <- train_groesse_50 + val_groesse_20
+
+train_data_3way <- Daten[1:train_groesse_50, ]
+val_data_3way <- Daten[(train_groesse_50 + 1):val_end, ]
+test_data_3way <- Daten[(val_end + 1):N, ]
